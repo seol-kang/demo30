28f9697191777d32b179be852c62eed6b920f2ef
ddarahakit2025 <ddarahakit2025@gmail.com>
2025-09-10 10:10:10 +0900

    first commit

---
+++ src/main/java/com/example/demo30/TestController.java
@@ -0,0 +1,13 @@
+package com.example.demo30;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/test")
+    public ResponseEntity test() {
+        return ResponseEntity.ok("테스트");
+    }
+}
