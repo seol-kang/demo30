79092d2ac9ac95eb4decff3a758b4ff1b1f69d21
ddarahakit2025 <ddarahakit2025@gmail.com>
2025-09-10 14:14:23 +0900

    test

--- src/main/java/com/example/demo30/TestController.java
+++ src/main/java/com/example/demo30/TestController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/test")
     public ResponseEntity test() {
-        return ResponseEntity.ok("테스트4");
+        return ResponseEntity.ok("테스트5");
     }
 }
