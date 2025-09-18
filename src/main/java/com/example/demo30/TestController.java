ab6a3eefb71afba45f54ecd5fc1c26ada87aa4d2
seol <norsors@nate.com>
2025-09-18 12:24:51 +0900

    feat:수정3

--- src/main/java/com/example/demo30/TestController.java
+++ src/main/java/com/example/demo30/TestController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/test")
     public ResponseEntity test() {
-        return ResponseEntity.ok("테스트05");
+        return ResponseEntity.ok("테스트06");
     }
 
     @GetMapping("/health")
